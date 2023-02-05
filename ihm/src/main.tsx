c4d9ddb4c57325598326a0e97cccc2f0870f104d
Artymaury <22880181+ArtyMaury@users.noreply.github.com>
2023-02-05 00:05:20 +0000

    simplification

--- ihm/src/main.tsx
+++ ihm/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Authenticated, AuthenticationProvider } from "./auth";
 import { CONFIG } from "./config";
 import "./index.css";
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <ServiceProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Navigate to="/admin" />} />
+            <Route path="/" element={<div>Accueil Partenaire</div>} />
             <Route path="/admin/*" element={<Authenticated />}>
               <Route index element={<Sponsors />} />
             </Route>
