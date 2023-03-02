f202cfbafbc78725819f54590585eac1ad84922f
ArtyMaury <arthur03.maury@gmail.com>
2023-03-02 09:06:40 +0100

    oups accueil

--- ihm/src/partenaires/accueil.tsx
+++ ihm/src/partenaires/accueil.tsx
@@ -1,13 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const Accueil: React.FC = () => {
-  const [map, setMap] = useState<string | null>(null);
-  React.useEffect(() => {
-    fetch("/map1-convertico.svg")
-      .then((res) => res.text())
-      .then(setMap);
-  }, []);
-  return <div dangerouslySetInnerHTML={{ __html: map ?? "" }}></div>;
-  // window.location.href = "https://devfest2023.gdgnantes.com";
-  // return null;
+  window.location.href = "https://devfest2023.gdgnantes.com";
+  return null;
 };
